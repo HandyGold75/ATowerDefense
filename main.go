d79f60319a0126e8e1d8b57544437100b3bf44f0
HandyGold75 <IZO@HandyGold75.com>
2025-06-12 12:55:43 +0200

    12-Jun-25

--- main.go
+++ main.go
@@ -2,15 +2,11 @@ package main
 
 import (
 	"ATowerDefense/client"
-	"ATowerDefense/game"
 	"ATowerDefense/server"
 	"fmt"
 	"os"
-	"strconv"
-	"time"
 
 	"github.com/HandyGold75/GOLib/argp"
-	"github.com/HandyGold75/GOLib/tui"
 )
 
 var args = argp.ParseArgs(struct {
@@ -18,46 +14,8 @@ var args = argp.ParseArgs(struct {
 	Server bool   `switch:"s,-server"                  help:"Start as a server instace."`
 	IP     string `switch:"i,-ip" default:"0.0.0.0"    help:"Listen on this ip when started as server."`
 	Port   uint16 `switch:"p,-port" default:"17540"    help:"Listen on this port when started as server."`
-	OpenGL bool   `switch:"o,-opengl"                  help:"Use OpenGL renderer"`
+	TUI    bool   `switch:"t,-tui"                  help:"Use TUI renderer"`
 }{})
-
-func menu() (gc game.GameConfig, err error) {
-	mode := ""
-
-	tui.Defaults.Align = tui.AlignLeft
-	mm := tui.NewMenuBulky("ASnake")
-
-	sp := mm.Menu.NewMenu("SinglePlayer")
-	sp.NewAction("Start", func() { mode = "singleplayer" })
-	spFieldHeight := sp.NewDigit("Field height", 50, 10, 9999)
-	spFieldWidth := sp.NewDigit("Field width", 50, 10, 9999)
-
-	mp := mm.Menu.NewMenu("MultiPlayer")
-	mp.NewAction("Connect", func() { mode = "multiplayer" })
-	mpIP := mp.NewIPv4("IP", "84.25.253.77")
-	mpPort := mp.NewDigit("Port", 17540, 0, 65535)
-
-	if err := mm.Run(); err != nil {
-		return gc, err
-	}
-
-	gc.Mode = mode
-	gc.IP = mpIP.Value()
-
-	port, err := strconv.ParseUint(mpPort.Value(), 10, 16)
-	if err != nil {
-		return gc, err
-	}
-	gc.Port = uint16(port)
-
-	if gc.FieldHeight, err = strconv.Atoi(spFieldHeight.Value()); err != nil {
-		return gc, err
-	}
-	if gc.FieldWidth, err = strconv.Atoi(spFieldWidth.Value()); err != nil {
-		return gc, err
-	}
-	return gc, nil
-}
 
 func main() {
 	if args.Server {
@@ -68,27 +26,7 @@ func main() {
 		os.Exit(0)
 	}
 
-	// gc, err := menu()
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	os.Exit(1)
-	// }
-
-	gc := game.GameConfig{
-		Mode:        "singleplayer",
-		IP:          "84.25.253.77",
-		Port:        17540,
-		FieldHeight: 15,
-		FieldWidth:  15,
-		TickDelay:   time.Millisecond * 50,
-	}
-
-	handler := "tui"
-	if args.OpenGL {
-		handler = "sdl"
-	}
-
-	if err := client.Run(gc, handler); err != nil {
+	if err := client.Run(args.TUI); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
